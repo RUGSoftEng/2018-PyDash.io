0e5acbf5852791958cee47da7acacb8a1ddfbb9c
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-06 20:06:26 +0100

    Render basic template

--- pydash/app/routes.py
+++ pydash/app/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, world!"
+    user = {'username': 'Qqwy'}
+    return render_template('index.html', title='Home', user=user)
