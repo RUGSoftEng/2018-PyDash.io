fee64a946d5ce3d0ac21906902c824569cf7a6ac
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-07 19:46:36 +0100

    using url_for

--- pydash/app/routes.py
+++ pydash/app/routes.py
@@ -1,9 +1,8 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
 @app.route("/index")
-@app.route("/dashboard")
 def index():
     return render_template('index.html', title='Home')
 
@@ -14,6 +13,11 @@ def login_post():
     login_form = LoginForm();
     if login_form.validate_on_submit():
         flash("User attempted to login: {}".format(login_form.username.data))
-        return redirect("/dashboard")
+        return redirect(url_for("dashboard"))
     else:
         return render_template("login.html", title="Sign In", login_form=login_form)
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html');
