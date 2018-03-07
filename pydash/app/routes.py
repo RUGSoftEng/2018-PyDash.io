df1b646513d8ff5590959098aee6b02584e7794b
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-07 19:42:15 +0100

    flash message on form submit

--- pydash/app/routes.py
+++ pydash/app/routes.py
@@ -1,15 +1,19 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
 @app.route("/index")
+@app.route("/dashboard")
 def index():
-    user = {'username': 'Qqwy'}
-    return render_template('index.html', title='Home', user=user)
+    return render_template('index.html', title='Home')
 
 
 @app.route("/")
-@app.route("/login")
-def login():
+@app.route("/login", methods=["GET", "POST"])
+def login_post():
     login_form = LoginForm();
-    return render_template("login.html", title="Sign In", login_form=login_form)
+    if login_form.validate_on_submit():
+        flash("User attempted to login: {}".format(login_form.username.data))
+        return redirect("/dashboard")
+    else:
+        return render_template("login.html", title="Sign In", login_form=login_form)
