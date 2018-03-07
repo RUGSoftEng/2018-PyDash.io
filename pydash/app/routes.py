402fe7cf69d259456cb56f135e009431bd708928
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-07 19:34:27 +0100

    creating login form

--- pydash/app/routes.py
+++ pydash/app/routes.py
@@ -1,8 +1,15 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
-@app.route("/")
 @app.route("/index")
 def index():
     user = {'username': 'Qqwy'}
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route("/")
+@app.route("/login")
+def login():
+    login_form = LoginForm();
+    return render_template("login.html", title="Sign In", login_form=login_form)
