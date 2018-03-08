035290ec6be682a3d5a34f6b74c27d56323875fe
Dunnkers <jeroenoverschie@gmail.com>
2018-03-08 12:35:01 +0100

    Add one textfield example ✌🏻

--- pydash-front/src/App.js
+++ pydash-front/src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
 
 let name = 'hhaha'
 
 class App extends Component {
+  state = {
+    name: 'Jeroen Overschie'
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
   render() {
     return (
@@ -21,6 +31,16 @@ class App extends Component {
         <Button variant="raised" color="primary">
           Hello World
         </Button>
+
+        <br/>
+        <TextField
+          id="name"
+          label="Name"
+          value={this.state.name}
+          onChange={this.handleChange('name')}
+          margin="normal"
+        />
+        {this.state.name}
       </div>
     );
   }
