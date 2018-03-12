ed69ecbae8beba53d41f4c1611373b8b59c69f30
Dunnkers <jeroenoverschie@gmail.com>
2018-03-12 19:32:50 +0100

    Add a basic login form ✅ No functionality yet.

--- pydash-front/src/App.js
+++ pydash-front/src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 
-let name = 'hhaha'
-
 class App extends Component {
   state = {
-    name: 'Jeroen Overschie'
+    username: '',
+    password: ''
   };
 
-  handleChange = name => event => {
+  handleChange = key => event => {
     this.setState({
-      [name]: event.target.value,
+      [key]: event.target.value
     });
   };
 
@@ -21,26 +19,32 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">PyDash.io Login page</h1>
+          <h1 className="App-title">PyDash.io Login</h1>
         </header>
-        <p className="App-intro">
-          Example react page ⚡️
-          {name}
-        </p>
-        <Button variant="raised" color="primary">
-          Hello World
-        </Button>
-
-        <br/>
-        <TextField
-          id="name"
-          label="Name"
-          value={this.state.name}
-          onChange={this.handleChange('name')}
-          margin="normal"
-        />
-        {this.state.name}
+        <form>
+          <br/>
+          <TextField
+            id="username"
+            label="Username"
+            value={this.state.username}
+            onChange={this.handleChange('username')}
+            margin="normal"
+          />
+          <br/>
+          <TextField
+            id="password"
+            label="Password"
+            value={this.state.password}
+            onChange={this.handleChange('password')}
+            margin="normal"
+            type="password"
+          />
+          <p>
+            <Button variant="raised" color="primary">
+              Login
+            </Button>
+          </p>
+        </form>
       </div>
     );
   }
