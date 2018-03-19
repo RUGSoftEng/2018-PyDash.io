8b16847aacc6855735759f8d19823e9ee150907c
dunnkers <jeroenoverschie@gmail.com>
2018-03-19 17:05:03 +0100

    Add Dashboard classes 💎 Just adding some directory structure.

---
+++ pydash-front/src/dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Dashboard.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <header className="App-header">
+                    <h1 className="App-title">Dashboard</h1>
+                </header>
+
+            </div>
+        );
+    }
+}
+
+export default Dashboard;
