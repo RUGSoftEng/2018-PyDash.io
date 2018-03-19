45ecadc9b31f0b6a54c0e415b5aa104411be333e
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-19 12:29:16 +0100

    fix some of tom's comments

--- pydash/pydash_app/user/user.py
+++ pydash/pydash_app/user/user.py
@@ -21,12 +21,13 @@ class User(persistent.Persistent, flask_login.UserMixin):
         if not isinstance(name, str) or not isinstance(password, str):
             raise TypeError("User expects name and password to be strings.")
 
-        self.id = uuid.uuid4() # str(id(self))
+        self.id = uuid.uuid4()  # str(id(self))
         self.name = name
         self.password_hash = generate_password_hash(password)
 
     def __repr__(self):
-        return '<{} id={} name={}>'.format(self.__class__.__name__, self.id, self.name)
+        return '<{} id={} name={}>'.format(self.__class__.__name__, self.id,
+                                           self.name)
 
     # TODO Use an ID that is separate from the user's name.
     def get_id(self):
@@ -34,7 +35,6 @@ class User(persistent.Persistent, flask_login.UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
-
 
     def __lt__(self, other):
         self.id < other.id
