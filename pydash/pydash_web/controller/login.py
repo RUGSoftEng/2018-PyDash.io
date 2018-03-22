e7bc2984597642b1ee32194cb432de0c8af5b309
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-03-22 12:40:59 +0100

    Replace json.dumps by flask.jsonify

--- pydash/pydash_web/controller/login.py
+++ pydash/pydash_web/controller/login.py
@@ -2,11 +2,9 @@
 Manages the logging in of a user into the application,
 and rejecting visitors that enter improper sign-in information.
 """
-import json
-
+from flask import jsonify
 from flask_login import current_user, login_user
 from flask_restplus.reqparse import RequestParser
-from werkzeug.exceptions import Unauthorized
 
 import pydash_app.user
 
@@ -14,25 +12,25 @@ import pydash_app.user
 def login():
     if current_user.is_authenticated:
         result = {"message": "User already logged in"}
-        return json.dumps(result)
+        return jsonify(result)
 
     args = __parse_arguments()
 
     if 'username' not in args or 'password' not in args:
         result = {"message": "Username or password missing"}
-        return json.dumps(result), 400
+        return jsonify(result), 400
 
     user = pydash_app.user.authenticate(args['username'],
                                         args['password'])
 
     if not user:
         result = {"message": "Username or password incorrect"}
-        return json.dumps(result), 401
+        return jsonify(result), 401
 
     login_user(user)
 
     result = {"message": "User successfully logged in"}
-    return json.dumps(result)
+    return jsonify(result)
 
 
 def __parse_arguments():
--- pydash/pydash_web/controller/logout.py
+++ pydash/pydash_web/controller/logout.py
@@ -1,17 +1,16 @@
 """
 Allows a user to sign out again after finishing using the application
 """
-import json
-
+from flask import jsonify
 from flask_login import current_user, logout_user
 
 
 def logout():
     if not current_user.is_authenticated:
         result = {"message": "User was not logged in"}
-        return json.dumps(result), 401
+        return jsonify(result), 401
 
     logout_user()
 
     result = {"message": "User successfully logged out"}
-    return json.dumps(result)
+    return jsonify(result)
