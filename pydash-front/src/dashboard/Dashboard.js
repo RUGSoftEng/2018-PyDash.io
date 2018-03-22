390e1da8556f569cfa283ef29a84786ada7bf067
dunnkers <jeroenoverschie@gmail.com>
2018-03-22 11:51:55 +0100

    Fix small CSS issues in Drawer & Bar 🔨 Lolsz 👌🏻

--- pydash-front/src/dashboard/Dashboard.js
+++ pydash-front/src/dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './Dashboard.css';
 import { withStyles } from 'material-ui/styles';
-import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
-import IconButton from 'material-ui/IconButton';
-import MenuIcon from 'material-ui-icons/Menu';
+// import AppBar from 'material-ui/AppBar';
+// import Toolbar from 'material-ui/Toolbar';
+// import Typography from 'material-ui/Typography';
+// import Button from 'material-ui/Button';
+// import IconButton from 'material-ui/IconButton';
+// import MenuIcon from 'material-ui-icons/Menu';
 import ResponsiveDrawer from './ResponsiveDrawer';
 
 const styles = {
--- pydash-front/src/dashboard/ResponsiveDrawer.js
+++ pydash-front/src/dashboard/ResponsiveDrawer.js
@@ -17,7 +17,8 @@ const drawerWidth = 240;
 const styles = theme => ({
     root: {
         flexGrow: 1,
-        height: 430,
+        // height: 430,
+        height: '100%',
         zIndex: 1,
         overflow: 'hidden',
         position: 'relative',
