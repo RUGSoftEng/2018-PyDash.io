cb5aa562fb3e89b77dc10453eadb6134062178b5
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-03-24 00:44:25 +0100

    Add function to allow for dynamic dashboard api endpoint names

--- pydash/pydash_app/dashboard/fetch.py
+++ pydash/pydash_app/dashboard/fetch.py
@@ -3,14 +3,20 @@ import jwt
 import json
 
 
+DETAILS_ENDPOINT = 0
+RULES_ENDPOINT = 1
+DATA_ENDPOINT = 2
+
+
 def get_details(dashboard_url):
     """
     Get details from a deployed flask-monitoring-dashboard
     :param dashboard_url: The base URL for the deployed dashboard, without trailing slash
     :return: A dict containing details from the dashboard, or None if the request was unsuccessful
     """
-    endpoint = 'get_json_details'
-    response = requests.get(f'{dashboard_url}/{endpoint}')
+    endpoint = _endpoint_name(DETAILS_ENDPOINT)
+
+    response = requests.get(f'{dashboard_url}/{_endpoint_name()}')
 
     if response.status_code != 200:
         return None
@@ -25,7 +31,8 @@ def get_monitor_rules(dashboard_url, dashboard_token):
     :param dashboard_token: The secret token for the dashboard, used to decode the Json Web Token response
     :return: A dict containing monitor rules of the dashboard, or None if the request was unsuccessful
     """
-    endpoint = 'get_json_monitor_rules'
+    endpoint = _endpoint_name(RULES_ENDPOINT)
+
     response = requests.get(f'{dashboard_url}/{endpoint}')
 
     if response.status_code != 200:
@@ -42,7 +49,8 @@ def get_data(dashboard_url, dashboard_token, time_from=None):
     :param time_from: An optional timestamp string indicating only data since that timestamp should be included
     :return: A dict containing all monitoring data or data since a given timestamp
     """
-    endpoint = 'get_json_data'
+    endpoint = _endpoint_name(DATA_ENDPOINT)
+
     url = f'{dashboard_url}/{endpoint}'
     if time_from is not None:
         url = f'{url}/{time_from}'
@@ -53,6 +61,15 @@ def get_data(dashboard_url, dashboard_token, time_from=None):
         return None
 
     return _decode_jwt(response.text, dashboard_token)
+
+
+def _endpoint_name(endpoint):
+    names = [
+        'get_json_details',
+        'get_json_monitor_rules',
+        'get_json_data'
+    ]
+    return names[endpoint]
 
 
 def _decode_jwt(payload, token):
