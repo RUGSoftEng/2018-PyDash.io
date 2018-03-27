a08dca4a2a3d92a57d292391730ef26cc0d78708
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-27 09:10:33 +0200

    Background tasks, updating tasks

--- pydash/pydash_app/impl/periodic_tasks.py
+++ pydash/pydash_app/impl/periodic_tasks.py
@@ -1,8 +1,31 @@
+"""
+Allows for the running of tasks in the background.
+
+Still to be done:
+
+- replacing/updating tasks when task with same name as existing task is added.
+- deleting tasks based on name.
+- wrapping default scheduler as 'global', which can be used as default for function decorators.
+- said function decorators.
+- background tasks besides the periodic tasks that only run once (partial support already exists, but no public calls to add them yet).
+- refactoring :D
+
+Example code:
+
+    >>> import pydash_app.impl.periodic_tasks as pt
+    >>> ts = pt.TaskScheduler()
+    >>> import datetime
+    >>> ts.run()
+    >>> ts.add_periodic_task('foo', datetime.timedelta(seconds=1), pt.foo)
+    >>> ts.add_periodic_task('bar', datetime.timedelta(seconds=5), pt.bar)
+"""
+
 import datetime
 from time import sleep
 import multiprocessing
-# from queue import PriorityQueue
-import heapq
+import queue
+# import heapq
+import heapy
 
 class Task:
     """
@@ -19,15 +42,20 @@ class Task:
         self.run_at_start = run_at_start
 
         self.next_run_dt = datetime.datetime.now()
-        if not self.run_at_start:
+        if self.interval is not None and not self.run_at_start:
             self.next_run_dt += interval
 
-    def __lt__(self, other):
-        return self.next_run_dt.__le__(other.next_run_dt)
+    # def __lt__(self, other):
+    #     return self.next_run_dt.__le__(other.next_run_dt)
 
     def __call__(self, *args, **kwargs):
         self.target(*args, **kwargs)
 
+    def __hash__(self):
+        return hash(self.name)
+
+    def __eq__(self, other):
+        return isinstance(other, Task) and other.name == self.name
 
 
 class TaskScheduler:
@@ -36,65 +64,99 @@ class TaskScheduler:
 
         :granularity: How often the scheduler should check if a periodic task's timeout has passed, in seconds. Defaults to `1.0`.
         """
-        self._task_queue = []
+        self._task_queue = heapy.pqueue()
         self._pool = None
         self._granularity = granularity
+        self._tasks_to_be_scheduled = multiprocessing.Queue()
 
     def add_periodic_task(self, name, interval, task):
+        """
+        Altering an already-existing task can be done
+        by calling this function with the new information but use the same name.
+        """
         # TODO input checking:
         # name: string
         # interval: timedelta
-        self._add_periodic_task(name, interval, task)
+        self._add_task(name, interval, task)
+
+    def add_background_task(self, name, task):
+        self._add_task(name, None, task)
+
+    def remove_task(self, name):
+        self._add_task(name, None, None)
 
     def run(self):
+        if hasattr(self, '_scheduler_process'):
+            raise Exception("TaskScheduler.run() called multiple times.")
         import atexit
         self._scheduler_process = multiprocessing.Process(target=self._run, daemon=False)
         # Ensure scheduler quits alongside main program
-        atexit.register(self._scheduler_process.terminate)
+        atexit.register(self.stop)
 
         self._scheduler_process.start()
 
     def stop(self):
-        if not hasattr(self, _scheduler_process):
-            raise Exception("`TaskScheduler.stop()` called before calling `TaskScheduler.start()`")
+        if not hasattr(self, '_scheduler_process'):
+            raise Exception("`TaskScheduler.stop()` called before calling `TaskScheduler.run()`")
         proc.terminate()
 
-    def _add_periodic_task(self, name, interval, task):
-        # TODO use inter-process abstraction
-        next_run_dt = datetime.datetime.now() + interval
-        # heapq.heappush(self._task_queue, (next_run_dt, interval, name, task))
-        heapq.heappush(self._task_queue, Task(name, task, interval=interval))
+    def _add_task(self, name, interval, task):
+        self._tasks_to_be_scheduled.put(Task(name, task, interval=interval))
+
+    def _add_tasks_to_be_scheduled(self):
+        print(f"Looking for tasks to add...")
+        print(f"approx. size: {self._tasks_to_be_scheduled.qsize()}")
+        try:
+            while True:
+                task = self._tasks_to_be_scheduled.get_nowait()
+                if task.target == None:
+                    print(f"Removing task {task.name} from internal pqueue")
+                    del self._task_queue[task]
+                else:
+                    print(f"Adding task {task.name} to internal pqueue")
+                    # heapq.heappush(self._task_queue, task)
+                    self._task_queue[task] = task.next_run_dt
+        except queue.Empty:
+            # Thrown by self._tasks_to_be_scheduled.get_nowait()
+            # when this cross-process queue happens to be empty.
+            pass
 
     def _run(self):
         with multiprocessing.Pool() as pool:
             while True:
+                self._add_tasks_to_be_scheduled()
                 current_time = datetime.datetime.now()
                 print(f"current time is: {current_time}")
                 self._run_tasks_that_should_have_run(pool, current_time)
                 sleep(self._granularity)
 
     def _run_tasks_that_should_have_run(self, pool, current_time):
-        # Do nothing if empty
-        if not self._task_queue:
-            return
         # Otherwise, iterate while tasks are before current time.
         # TODO restructure into for-comprehension?
         while True:
-            task =  self._task_queue[0]
+            print(f"task queue: {self._task_queue}")
+
+            # Do nothing if empty
+            if not self._task_queue:
+                return
+
+            task, _ =  self._task_queue.peek()
             print(f"Looking at task {task.name} which should be run at {task.next_run_dt}, every {task.interval}")
             if task.next_run_dt < current_time:
                 print(f"Running task {task.name}...")
-                heapq.heappop(self._task_queue)
+                # heapq.heappop(self._task_queue)
+                self._task_queue.pop()
                 self._run_task(pool, task)
-                if task.interval != None:
+                if task.interval is not None:
                     # self._add_periodic_task(task.name, task.interval, tasktask)
                     task.next_run_dt = datetime.datetime.now() + task.interval
-                    heapq.heappush(self._task_queue, task)
+                    # heapq.heappush(self._task_queue, task)
+                    self._task_queue[task] = task.next_run_dt
             else:
                 break
 
     def _run_task(self, pool, task):
-        res = pool.apply_async(task.target, ())
+        res = pool.apply_async(task, ())
         res.get()
 
 
@@ -109,3 +171,6 @@ def foo():
 
 def bar():
     print('bar')
+
+def baz():
+    print('baz')
