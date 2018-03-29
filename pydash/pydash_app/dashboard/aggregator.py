f64352b5e080ccdfd1a2289547ec999b8105f753
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-03-29 17:23:26 +0200

    Add average execution time and visits per day metrics

--- pydash/pydash_app/dashboard/aggregator.py
+++ pydash/pydash_app/dashboard/aggregator.py
@@ -18,6 +18,10 @@ class Aggregator(persistent.Persistent):
         self.total_visits = 0
         self.total_execution_time = 0
 
+        self.average_execution_time = None
+
+        self.visits_per_day = {}
+
     def add_endpoint_call(self, endpoint_call):
         """
         Add an endpoint call and update aggregated data
@@ -29,6 +33,15 @@ class Aggregator(persistent.Persistent):
         self.total_visits += 1
         self.total_execution_time += endpoint_call.execution_time
 
+        self.average_execution_time = self.total_execution_time / len(self._endpoint_calls)
+
+        day = endpoint_call.time.strftime('%Y-%m-%d')
+
+        if day in self.visits_per_day:
+            self.visits_per_day[day] += 1
+        else:
+            self.visits_per_day[day] = 1
+
     def as_dict(self):
         """
         Get aggregated data in a dict
@@ -38,4 +51,6 @@ class Aggregator(persistent.Persistent):
         return {
             'total_visits': self.total_visits,
             'total_execution_time': self.total_execution_time,
+            'average_execution_time': self.average_execution_time,
+            'visits_per_day': self.visits_per_day
         }
