65669e79d03c115ffc71d8c89b8b3e4314ae218f
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-03-30 21:35:31 +0200

    improves backend CORS headers

--- pydash/pydash_web/routes.py
+++ pydash/pydash_web/routes.py
@@ -9,7 +9,7 @@ from flask_cors import CORS
 
 from pydash_web.blueprint import bp
 import pydash_web.controller as controller
-cors = CORS(bp, resources={r"/api/*": {"origins": "*"}}) # Only keep this during development!
+cors = CORS(bp, resources={r"/api/*": {"origins": "*"}}, supports_credentials=True) # Only keep this during development!
 
 @bp.route("/")
 def serve_react():
