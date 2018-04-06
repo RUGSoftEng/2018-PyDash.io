aae2261850931c2aa524d971f71de00e27a62bfd
dunnkers <jeroenoverschie@gmail.com>
2018-04-06 23:48:30 +0200

    Fix linting issues; remove dead code 🔨

--- pydash-front/src/app/overview/DashTileGrid.js
+++ pydash-front/src/app/overview/DashTileGrid.js
@@ -16,28 +16,6 @@ const styles = theme => ({
   },
 });
 
-
-// Transforms a hashmap of key-value pairs into an array of {x: key, y: value} objects.
-function dict_to_xy_arr(dict){
-    let res =  Object.entries(dict).map(function([key, value]){
-        return {x: key, y: value}
-    });
-    console.log('dict_to_xy_array', res);
-    return res;
-}
-
-/* function destringify_date_keys(dict){
- *     let res =  Object.entries(dict).map(function([key, value]){
- *         let obj = {}
- *         obj[new Date(key)] = value;
- *         return obj;
- *     })
- *     .reduce((acc, kv) => Object.assign(acc, kv), {})
- *     console.log('destringify_date_keys', res);
- *     return res;
- * }
- * */
-
 class DashTileGrid extends Component {
     constructor(props) {
         super(props);
@@ -46,35 +24,6 @@ class DashTileGrid extends Component {
             dashboards: [],
         };
     }
-
-    /*componentDidMount() {
-        axios('http://localhost:5000/api/dashboards/123', {
-            method: 'get',
-            withCredentials: true
-        }).then((response) => {
-            console.log('success', response);
-            this.setState(prevState =>{
-                let newState = prevState;
-                newState.total_visits = "" + response.data.aggregates.total_visits;
-                newState.visits_per_day = dict_to_xy_arr(response.data.aggregates.visits_per_day)
-                console.log(newState)
-
-                return newState;
-            })
-        }).catch((error) => {
-            console.log('error', error);
-
-            /* this.setState(prevState =>{
-             *     let newState = prevState;
-             *     newState.total_visits = "" + mock_data.aggregates.total_visits;
-             *     newState.visits_per_day = dict_to_xy_arr(mock_data.aggregates.visits_per_day)
-             *     console.log(newState)
-
-             *     return newState;
-             * })
-             */
-        /*});
-    }*/
     
     componentDidMount() {
       axios('http://localhost:5000/api/dashboards', {
