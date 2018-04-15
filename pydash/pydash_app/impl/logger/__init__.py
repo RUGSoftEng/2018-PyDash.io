92ce0cb60b7c6942a556fdfd2ec94842892d9cbe
TheVeggydude <jeroenlanghorst@gmail.com>
2018-04-15 15:13:03 +0200

    /logger/__init__.py now creates the new logger object for global usage

--- pydash/pydash_app/impl/logger/__init__.py
+++ pydash/pydash_app/impl/logger/__init__.py
@@ -1,24 +1,27 @@
 """
-Creates a default_logger for use in the entire project. This logger will log messages and errors to dated .log files in
-the /logs directory of the project.
+Creates a default_logger for use in the entire project. This logger will log messages and errors to date-stamped .log
+files in the /logs directory of the project.
 """
-import logging
-import datetime
-import pathlib
-import os
+from .logger import Logger
 
-"""Default settings"""
-logging.basicConfig(level=logging.INFO)
+logger = Logger()
 
-default_logger = logging.getLogger(__name__)
-default_handler = logging.FileHandler(os.getcwd() + '/logs/' + str(datetime.datetime.today().date()) + '.log')
-default_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-default_handler.setFormatter(default_formatter)
-default_logger.addHandler(default_handler)
-
-default_logger.info('Initializing logger done')
-
-def log():
-
-    pass
+# import logging
+# import datetime
+# import os
+#
+# """Default settings"""
+# logging.basicConfig(level=logging.INFO)
+#
+# default_logger = logging.getLogger(__name__)
+# default_handler = logging.FileHandler(os.getcwd() + '/logs/' + str(datetime.datetime.today().date()) + '.log')
+# default_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#
+# default_handler.setFormatter(default_formatter)
+# default_logger.addHandler(default_handler)
+#
+# default_logger.info('Initializing logger done')
+#
+# def log():
+#
+#     pass
