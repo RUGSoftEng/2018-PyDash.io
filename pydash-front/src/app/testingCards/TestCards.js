20d4a7b010f3d868b23f037dcfe376293e65c3ca
Alberto <alberto@RN-145-97-156-150.eduroam.rug.nl>
2018-04-24 14:46:23 +0200

    Wrench FTW

--- pydash-front/src/app/testingCards/TestCards.js
+++ pydash-front/src/app/testingCards/TestCards.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Card from 'material-ui/Card'
 import {CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
-import CreateIcon from 'material-ui-icons/Create';
+import BuildIcon from 'material-ui-icons/Build';
 import { NavLink } from 'react-router-dom'
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Grid from 'material-ui/Grid';
@@ -14,7 +14,7 @@ class TestCards extends Component {
               <CardActions>
               <ListItem button component={NavLink} to={'/dashboard/settings'}>
                 <ListItemIcon>
-                    <CreateIcon />
+                    <BuildIcon />
                 </ListItemIcon>
             </ListItem>
                 </CardActions>
