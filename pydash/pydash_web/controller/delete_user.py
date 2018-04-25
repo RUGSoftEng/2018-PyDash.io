24ed54bbd6907507dc6f7aa7c1bb2705691ab467
TWEApol <tom142606@gmail.com>
2018-04-26 00:32:13 +0200

    More refactoring.

--- pydash/pydash_web/controller/delete_user.py
+++ pydash/pydash_web/controller/delete_user.py
@@ -5,7 +5,7 @@ Manages deletion of a user.
 from flask import jsonify
 from flask_login import current_user
 
-from pydash_app.user import maybe_find_user, remove_from_repository as remove_from_user_repository
+from pydash_app.user import find, remove_from_repository as remove_from_user_repository
 from pydash_app.dashboard import dashboards_of_user, remove_from_repository as remove_from_dashboard_repository
 import pydash_app.impl.logger as pylog
 
@@ -17,26 +17,21 @@ def delete_user():
     Deletes the currently logged in user and all dashboards they own.
     """
 
-    maybe_user = maybe_find_user(current_user.id)
-    if maybe_user is None:
+    try:
+        user = find(current_user.id)
+        remove_from_user_repository(user)
+    except KeyError or UnboundLocalError:  # UnboundLocalError will be thrown as well, due to the use of `user`.
         result = {'message': 'User not found in database.'}
         logger.warning(f'Delete_user failed - {current_user} was not found in the database.')
-        return jsonify(result), 500
-
-    try:  # In case the database has been updated right in between the previous check.
-        remove_from_user_repository(maybe_user)
-    except KeyError:
-        result = {'message': 'User not found in database.'}
-        logger.warning(f'Delete_user failed - {maybe_user} was not found in the database.')
         return jsonify(result), 500
 
     for dashboard in dashboards_of_user(current_user.id):
         try:
             remove_from_dashboard_repository(dashboard)
         except KeyError:
-            logger.warning(f'Dashboard {dashboard} from user {maybe_user} has already been removed.')
+            logger.warning(f'Dashboard {dashboard} from user {user} has already been removed.')
 
-    logger.info(f'{current_user} deleted themselves successfully.')
+    logger.info(f'{user} deleted themselves successfully.')
 
     result = {'message': 'User successfully deleted themselves.'}
     return jsonify(result), 200
