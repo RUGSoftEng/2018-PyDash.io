51512b8f8a8cd4c5dfc31c01c167ace92900af2e
Arjan Tilstra <a.tilstra.2@student.rug.nl>
2018-04-29 14:41:29 +0200

    Created dummy data for testing endpoints bar graph. Wrote converter function from api data to bar graph data.

--- pydash-front/src/app/boards/GraphGrid.js
+++ pydash-front/src/app/boards/GraphGrid.js
@@ -15,6 +15,19 @@ function dict_to_xy_arr(dict){
   return res;
 }
 
+// Transforms the returned endpoint data from the api into a form that can be
+// displayed in a nivo bar graph.
+function api_to_bar_data(endpoints) {
+  let res = [];
+  for (let i in endpoints) {
+    let name = endpoints[i].name;
+    let average_execution_time = endpoints[i].aggregates.average_execution_time;
+    res.push({'name': name, 'average_execution_time': average_execution_time});
+  }
+  console.log('bar data', res);
+  return res;
+}
+
 const styles = theme => ({
   root: {
     flexGrow: 1,
@@ -25,6 +38,43 @@ const styles = theme => ({
     color: theme.palette.text.secondary,
   },
 });
+
+// Dummy data for testing average endpoint execution time visualisation.
+const endpoints = {
+  '0': {
+    'aggregates': {
+      'average_execution_time': 23
+    },
+    'name': "index"
+  },
+  '1': {
+    'aggregates': {
+      'average_execution_time': 48
+    },
+    'name': "user"
+  },
+  '2': {
+    'aggregates': {
+      'average_execution_time': 10
+    },
+    'name': "dashboard"
+  },
+  '3': {
+    'aggregates': {
+      'average_execution_time': 25
+    },
+    'name': "testpage"
+  },
+  '4': {
+    'aggregates': {
+      'average_execution_time': 2
+    },
+    'name': "overview"
+  },
+
+};
+
+
 
 class GraphGrid extends Component {
   constructor(props) {
@@ -50,6 +100,7 @@ class GraphGrid extends Component {
             newState.total_visits = "" + response.data.aggregates.total_visits;
             newState.visits_per_day = dict_to_xy_arr(response.data.aggregates.visits_per_day)
             newState.unique_visitors_per_day = dict_to_xy_arr(response.data.aggregates.unique_visitors_per_day)
+            newState.average_execution_times = api_to_bar_data(endpoints);
             console.log(newState);
 
             return newState;
