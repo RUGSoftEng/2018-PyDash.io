797c3b85081307a3a09aa7fdc67b947fd1a766f3
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-04-30 00:49:43 +0200

    Add two testing dashboard to each user
    One running FMD 1.12.0, one running 1.11.5

--- pydash/pydash_app/dashboard/services/seeding.py
+++ pydash/pydash_app/dashboard/services/seeding.py
@@ -10,23 +10,25 @@ import pydash_app.user.repository as user_repository
 
 import pydash_app.dashboard.services.fetching as fetching
 
+
 def seed():
     """
     For each user, stores some preliminary debug dashboards in the datastore,
     to be used during development.
-    Note: for now the two dashboards that are being returned are
-    identical, apart from the url.
     """
 
-    # Clear current Dashboards-DB.
     repository.clear_all()
 
     for user in user_repository.all():
-        dashboard = Dashboard("http://136.243.248.188:9001/dashboard",
-                              "cc83733cb0af8b884ff6577086b87909",
-                              user.get_id())
-        print(f'Adding dashboard {dashboard}')
-        repository.add(dashboard)
-        print(f'Fetching remote info for dashboard {dashboard}.')
-        fetching.fetch_and_update_historic_dashboard_info(dashboard.id)
+        dashboard_new = Dashboard('http://flask-sample.koenbolhuis.nl/dashboard',
+                                  'cc83733cb0af8b884ff6577086b87909',
+                                  user.get_id())
+        dashboard_old = Dashboard('http://flask-sample-old.koenbolhuis.nl/dashboard',
+                                  'cc83733cb0af8b884ff6577086b87909',
+                                  user.get_id())
+        for dashboard in [dashboard_new, dashboard_old]:
+            print(f'Adding dashboard {dashboard}')
+            repository.add(dashboard)
+            print(f'Fetching remote info for dashboard {dashboard}.')
+            fetching.fetch_and_update_historic_dashboard_info(dashboard.id)
     print('Seeding of dashboards is done!')
