68fe4244df43d56b8838ee418fd21aa4569afb3e
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-05-01 23:52:08 +0200

    Slightly clean up pydash_web

--- pydash/pydash_web/__init__.py
+++ pydash/pydash_web/__init__.py
@@ -18,26 +18,21 @@ import pydash_app
 import pydash_app.user
 
 
-flask_webapp = Flask(__name__,
-static_folder=None
-)
+flask_webapp = Flask(__name__, static_folder=None)
 flask_webapp.config.from_object(Config)
 
 flask_webapp.config['CORS_HEADERS'] = 'Content-Type'
-cors = CORS(flask_webapp, resources={r"/api/*": {"origins": "*"}}, allow_headers=['Content-Type'], supports_credentials=True) # Only keep this line during development!
+cors = CORS(flask_webapp,
+            resources={r"/api/*": {"origins": "*"}},
+            allow_headers=['Content-Type'],
+            supports_credentials=True) # Only keep this line during development!
 
 flask_webapp.register_blueprint(api_blueprint)
 flask_webapp.register_blueprint(react_server_blueprint)
 
-#pydash_app.schedule_periodic_tasks()
-
-
-# Don't autostart scheduler in the testing environment.
-print(os.environ)
-if 'TESTING' not in os.environ:
-    pydash_app.start_task_scheduler()
-
 login_manager = LoginManager(flask_webapp)
+
+
 @login_manager.user_loader
 def load_user(user_id):
     print("Loading user {}".format(user_id))
@@ -48,3 +43,12 @@ def load_user(user_id):
 def seed_command():
     """Initializes our datastore with some preliminary values"""
     pydash_app.seed_datastructures()
+
+
+# pydash_app.schedule_periodic_tasks()
+
+
+# Don't autostart scheduler in the testing environment.
+print(os.environ)
+if 'TESTING' not in os.environ:
+    pydash_app.start_task_scheduler()
