a02f9ad21a873e59c8adb43a1cc83fd6fe7449fa
Alberto <alberto@MacBook-Pro-de-Alberto.local>
2018-05-01 12:25:15 +0200

    test 12:25

--- pydash-front/src/accountCreation/AccountCreation.js
+++ pydash-front/src/accountCreation/AccountCreation.js
@@ -5,23 +5,33 @@ import TextField from 'material-ui/TextField';
 import axios from 'axios';
 import Logo from '../images/logo.png'
 
-class accountCreation extends Component {
-    state = {
+class accountCreation extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
         username: '',
-        password: '',
-        Confirm_password: '',
         email: '',
-        error: false,
+        password: '',
+        Confirmpassword: '',
         message: '',
+        error: false,
+        loading: false,
         success: false
-    };
+      }
 
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
     handleChange = key => event => {
         this.setState({
             [key]: event.target.value
         });
     };
 
+   
+   
+   
     tryLogin = (e) => {
         e.preventDefault()
         let username = this.state.username,
@@ -36,6 +46,12 @@ class accountCreation extends Component {
 
             return;
         }
+        this.setState(prevState => ({
+            ...prevState,
+                error: false,
+                helperText: '',
+                loading: true
+            }))
 
         axios.post(window.api_path + '/api/register_user', {
             username,
@@ -47,10 +63,18 @@ class accountCreation extends Component {
             this.setState(prevState => ({
                 error: false,
                 helperText: '',
-                success: true
+                success: true,
+                loading: false
             }));
         }).catch((error) => {
             console.log(error);
+            if(error.response && error.response.status === 409) {
+                this.setState(prevState => ({
+                    error: true,
+                    helperText: 'User already exists',
+                    loading: false,
+                }))
+            }
         });
     }
 
@@ -118,3 +142,121 @@ class accountCreation extends Component {
 }
 
 export default accountCreation;
+
+// import React from 'react';
+// import TextField from 'material-ui/TextField';
+
+// class SignupForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       username: '',
+//       email: '',
+//       password: '',
+//       passwordConfirmation: '',
+//       errors: {},
+//       isLoading: false,
+//       invalid: false
+//     }
+
+//     this.onChange = this.onChange.bind(this);
+//     this.onSubmit = this.onSubmit.bind(this);
+//     this.checkUserExists = this.checkUserExists.bind(this);
+//   }
+
+//   onChange(e) {
+//     this.setState({ [e.target.name]: e.target.value });
+//   }
+
+
+//   checkUserExists(e) {
+//     const field = e.target.name;
+//     const val = e.target.value;
+//     if (val !== '') {
+//       this.props.isUserExists(val).then(res => {
+//         let errors = this.state.errors;
+//         let invalid;
+//         if (res.data.user) {
+//           errors[field] = 'There is user with such ' + field;
+//           invalid = true;
+//         } else {
+//           errors[field] = '';
+//           invalid = false;
+//         }
+//         this.setState({ errors, invalid });
+//       });
+//     }
+//   }
+
+//   onSubmit(e) {
+//     e.preventDefault();
+
+//     if (this.isValid()) {
+//       this.setState({ errors: {}, isLoading: true });
+//       this.props.userSignupRequest(this.state).then(
+//         () => {
+//           this.props.addFlashMessage({
+//             type: 'success',
+//             text: 'You signed up successfully. Welcome!'
+//           });
+//           this.context.router.push('/');
+//         },
+//         (err) => this.setState({ errors: err.response.data, isLoading: false })
+//       );
+//     }
+//   }
+
+//   render() {
+//     const { errors } = this.state;
+//     return (
+//       <form onSubmit={this.onSubmit}>
+//         <h1>Join our community!</h1>
+
+//         <TextField
+//           error={errors.username}
+//           label="Username"
+//           onChange={this.onChange}
+//           checkUserExists={this.checkUserExists}
+//           value={this.state.username}
+//           field="username"
+//         />
+
+//         <TextField
+//           error={errors.email}
+//           label="Email"
+//           onChange={this.onChange}
+//           checkUserExists={this.checkUserExists}
+//           value={this.state.email}
+//           field="email"
+//         />
+
+//         <TextField
+//           error={errors.password}
+//           label="Password"
+//           onChange={this.onChange}
+//           value={this.state.password}
+//           field="password"
+//           type="password"
+//         />
+
+//         <TextField
+//           error={errors.passwordConfirmation}
+//           label="Password Confirmation"
+//           onChange={this.onChange}
+//           value={this.state.passwordConfirmation}
+//           field="passwordConfirmation"
+//           type="password"
+//         />
+
+//         <div className="form-group">
+//           <button disabled={this.state.isLoading || this.state.invalid} className="btn btn-primary btn-lg">
+//             Sign up
+//           </button>
+//         </div>
+//       </form>
+//     );
+//   }
+// }
+
+
+// export default SignupForm;
