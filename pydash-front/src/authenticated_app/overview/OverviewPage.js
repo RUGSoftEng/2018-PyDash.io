9601ba8a730b8989b083c43def01060980a48bfc
Arjan Tilstra <a.tilstra.2@student.rug.nl>
2018-05-05 17:17:58 +0200

    Dashboard creation form added to overview page. Currently nonfunctional because of backend limitations

--- pydash-front/src/authenticated_app/overview/OverviewPage.js
+++ pydash-front/src/authenticated_app/overview/OverviewPage.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
 // Button stuff
-import Add from '@material-ui/icons/Add'
+import Add from '@material-ui/icons/Add';
+
+// Form stuff
+import AddDashboardDialog from './AddDashboardDialog';
 
 // Contents:
 import DashboardList from './DashboardList';
@@ -9,15 +12,31 @@ import { Button } from 'material-ui';
 
 
 class OverviewPage extends Component {
+    state = {
+        open: false,
+    }
+
+    handleClickOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
     render() {
         return (
 
             <div>
                 <h2>Dashboards</h2>
-                <Button classname="NewDashboardButton" variant="raised" color="primary">
+                <Button onClick={this.handleClickOpen} classname="NewDashboardButton" variant="raised" color="primary">
                     Add dashboard
                     <Add />
                 </Button>
+                <AddDashboardDialog 
+                    open={this.state.open}
+                    onClose={this.handleClose}
+                />
                 <DashboardList dashboards={this.props.dashboards} />
             </div>
         );
