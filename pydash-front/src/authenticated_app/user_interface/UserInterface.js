20c5d9bfc22a82671e861b8983aee0d3b8a01758
Arjan Tilstra <arjan.98@live.nl>
2018-05-08 14:17:10 +0200

    Menu scrolls with the page

--- pydash-front/src/authenticated_app/user_interface/UserInterface.js
+++ pydash-front/src/authenticated_app/user_interface/UserInterface.js
@@ -85,7 +85,7 @@ class UserInterface extends React.Component {
         const { classes, theme } = this.props;
 
         const drawer = (
-            <div>
+            <div className="sidebar">
 
                 <div className={classes.toolbar} >
                 <UserIcon className={classes.accounticon}  />
