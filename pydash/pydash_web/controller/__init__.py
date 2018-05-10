b6da7eeba5c4602e3b053329187ca642453af117
TheVeggydude <jeroenlanghorst@gmail.com>
2018-05-10 12:03:53 +0200

    controller package now neatly imports register_dashboard

--- pydash/pydash_web/controller/__init__.py
+++ pydash/pydash_web/controller/__init__.py
@@ -8,3 +8,4 @@ from .dashboards import dashboards
 from .dashboards import dashboard
 from .register_user import register_user
 from .delete_user import delete_user
+from .register_dashboard import register_dashboard
