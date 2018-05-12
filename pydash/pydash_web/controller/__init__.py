f11d9f9889d4da6d628146708dad725657c33c08
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-05-12 19:11:33 +0200

    Slightly restructure pydash_web and setup dashboard deletion route

--- pydash/pydash_web/controller/__init__.py
+++ pydash/pydash_web/controller/__init__.py
@@ -4,10 +4,13 @@ The controller contains one dispatching function per flask_webapp endpoint actio
 
 from .login import login
 from .logout import logout
-from .dashboards import dashboards
-from .dashboards import dashboard
+
 from .register_user import register_user
 from .delete_user import delete_user
-from .register_dashboard import register_dashboard
 from .change_settings import change_settings
 from .change_password import change_password
+
+from .dashboards import dashboards
+from .dashboards import dashboard
+from .register_dashboard import register_dashboard
+from .delete_dashboard import delete_dashboard
---
+++ pydash/pydash_web/controller/delete_dashboard.py
@@ -0,0 +1,7 @@
+"""
+Manages the deletion of a dashboard.
+"""
+
+
+def delete_dashboard(dashboard_id):
+    pass
