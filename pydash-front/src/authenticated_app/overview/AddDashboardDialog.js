60e5cb1cc19b954ad5cea453c0e34c8b950ef6e1
Arjan Tilstra <a.tilstra.2@student.rug.nl>
2018-05-13 21:38:22 +0200

    First draft of dashboard creation working

--- pydash-front/src/authenticated_app/overview/AddDashboardDialog.js
+++ pydash-front/src/authenticated_app/overview/AddDashboardDialog.js
@@ -8,7 +8,93 @@ import Dialog, {
     DialogTitle,
 } from 'material-ui/Dialog';
 
+import axios from 'axios';
+
 class AddDashboardDialog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: '',
+            name: '',
+            token: '',
+            message: '',
+            error: false,
+            loading: false,
+            success: false,
+        }
+    }
+
+    handleChange = key => event => {
+        this.setState({
+            [key]: event.target.value
+        });
+    };
+
+    handleClick = () => {
+        this.setState({ open: true });
+    };
+
+    handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        this.setState({ open: false });
+    };
+
+    tryCreation = (e) => {
+        e.preventDefault();
+        let url = this.state.url,
+            name = this.state.name,
+            token = this.state.token;
+
+        if (!(url.trim()) || !(token.trim())) {
+            this.setState(prevState => ({
+                ...prevState,
+                error: true,
+                open: false,
+                helperText: 'These fields are required!',
+            }))
+
+            return;
+        }
+
+        this.setState(prevState => ({
+            ...prevState,
+            error: false,
+            helperText: '',
+            loading: true
+        }))
+
+        axios.post(window.api_path + '/api/dashboards/register', {
+            url,
+            name,
+            token},
+            {withCredentials: true}
+        ).then((response) => {
+            console.log(response);
+            this.setState(prevState => ({
+                ...prevState,
+                error: false,
+                helperText: '',
+                success: true,
+                loading: false,
+            }));
+        }).catch((err) => {
+            console.log(err);
+            console.log(err.response);
+            if(err.response && err.response.status === 400) {
+                this.setState(prevState => ({
+                    ...prevState,
+                    error: true,
+                    helperText: err.response.data.message,
+                    loading: false,
+                }));
+            }
+        });
+        
+    }
+
     render() {
         return (
             <div>
@@ -29,27 +115,34 @@ class AddDashboardDialog extends Component {
                             type="url"
                             fullWidth
                             required
+                            error={this.state.error}
+                            onChange={this.handleChange('url')}
                         />
                         <TextField
                             id="name"
                             label="Dashboard name"
                             type="text"
                             fullWidth 
+                            error={this.state.error}
+                            onChange={this.handleChange('name')}
                         />
                         <TextField 
-                            id="code"
-                            label="Security code"
+                            id="token"
+                            label="Security token"
                             type="password"
                             fullWidth 
                             required
+                            error={this.state.error}
+                            helperText={this.state.helperText}
+                            onChange={this.handleChange('token')}
                         />
                     </DialogContent>
                     <DialogActions>
-                        <Button onClick={this.props.onClose} color="#fff">
+                        <Button onClick={this.props.onClose} color="default">
                             Cancel
                         </Button>
-                        <Button onClick={this.props.onClose} color="primary">
-                            Save
+                        <Button onClick={this.tryCreation} color="primary" disabled={this.state.loading} variant="raised">
+                            {this.state.loading ? "Adding dashboard" : "Save"}
                         </Button>
                     </DialogActions>
                 </Dialog>
