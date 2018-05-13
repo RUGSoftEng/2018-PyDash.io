c483301bcb89cd7d080d906e3f6f48701ca3f9a5
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-05-13 22:23:05 +0200

    Fix response messages

--- pydash/pydash_web/controller/delete_dashboard.py
+++ pydash/pydash_web/controller/delete_dashboard.py
@@ -16,21 +16,21 @@ def delete_dashboard(dashboard_id):
         dashboard = pydash_app.dashboard.find(dashboard_id)
     except KeyError:
         logger.warning(f"Could not find dashboard matching with {dashboard_id}")
-        return jsonify({"message": "Could not find a matching dashboard."}), 404
+        return jsonify({"message": "Could not find a matching dashboard"}), 404
     except ValueError:  # Happens when called without a proper UUID
         logger.warning(f"Invalid dashboard_id: {dashboard_id}")
         return jsonify({"message": "Invalid dashboard_id"}), 400
 
     if dashboard.user_id != current_user.id:
         logger.warning(f"{current_user} is not authorised to view {dashboard}")
-        return jsonify({"message": "Not authorised to view this dashboard."}), 403
+        return jsonify({"message": "Not authorised to delete this dashboard"}), 403
 
     try:
         # TODO: only remove if owned by one user, otherwise just remove the user's id from the dashboard
         pydash_app.dashboard.remove_from_repository(dashboard)
     except KeyError:
         logger.warning(f'Dashboard {dashboard} does not seem to exist in the database')
-        jsonify({"message": "Could not find a matching dashboard."}), 404
+        jsonify({"message": "Could not find a matching dashboard"}), 404
 
     result = {'message': 'Successfully removed dashboard'}
     return jsonify(result), 200
