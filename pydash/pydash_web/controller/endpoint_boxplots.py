9a00371e6e62701a25a5ef77f2c24144dab6b8a2
TWEApol <tom142606@gmail.com>
2018-05-15 00:17:46 +0200

    Added dashboard_id and user permission checks.

--- pydash/pydash_web/controller/endpoint_boxplots.py
+++ pydash/pydash_web/controller/endpoint_boxplots.py
@@ -1,19 +1,33 @@
 
 from flask import jsonify
+from flask_login import current_user
 import pydash_app.dashboard
 import pydash_logger
+import pydash_app.dashboard.services
 
 
 logger = pydash_logger.Logger(__name__)
 
 
 def endpoint_boxplots(dashboard_id):
-    #TODO: check whether dashboard_id is valid.
-    #Todo: check whether user may view dashboard.
+    # Check whether dashboard_id is valid.
+    try:
+        dashboard = pydash_app.dashboard.find(dashboard_id)
+    except Exception:
+        result = {'message': f'Dashboard_id {dashboard_id} is invalid.'}
+        logger.warning(f'Endpoint_boxplots failure - Dashboard_id {dashboard_id} is invalid. '
+                       f'Current user: {current_user}')
+        return jsonify(result), 400
+
+    # Check whether user may view dashboard.
+    if str(dashboard.user_id) != str(current_user.id):
+        result = {'message': f'Current user is not allowed to access dashboard {dashboard_id}'}
+        logger.warning(f'Endpoint_boxplots failure - User {current_user} is not allowed to access dashboard {dashboard_id}')
+        return jsonify(result), 403
 
     dashboard = pydash_app.dashboard.find(dashboard_id)
     endpoint_boxplot_list = [_boxplot_data(endpoint) for endpoint in dashboard.endpoints.values()]
-    logger.info(f'Endpoint boxplots from dashboard {dashboard} requested')
+    logger.info(f'Endpoint boxplots successful -  data from dashboard {dashboard} requested')
     return jsonify(endpoint_boxplot_list), 200
 
 
