529209c00447a350d5bc337496ae6893c2dce1c9
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-05-14 16:54:28 +0200

    Remove Flask-RESTPlus from change_password

--- pydash/pydash_web/controller/change_password.py
+++ pydash/pydash_web/controller/change_password.py
@@ -2,9 +2,8 @@
 Manages changing of the user's password.
 """
 
-from flask import jsonify
+from flask import jsonify, request
 from flask_login import current_user
-from flask_restplus.reqparse import RequestParser
 
 import pydash_app.user.repository as user_repository
 import pydash_logger
@@ -17,28 +16,26 @@ def change_password():
 
     args = _parse_arguments()
 
-    if 'current_password' not in args or 'new_password' not in args:
+    current_password = request.args.get('current_password')
+    new_password = request.args.get('new_password')
+
+    if current_password is None or new_password is None:
         logger.warning('Password change failed - current password or new password missing')
         result = {'message': 'Current password or new password missing'}
         return jsonify(result), 400
-
-    current_password = args['current_password']
-    new_password = args['new_password']
 
     if not current_user.check_password(current_password):
         logger.warning('Password change failed - current password invalid')
         result = {'message': 'Current password incorrect'}
         return jsonify(result), 401
 
+    if not new_password:
+        logger.warning('Password change failed - new password cannot be empty')
+        result = {'message': 'New password cannot be empty'}
+        return jsonify(result), 400
+
     current_user.set_password(new_password)
     user_repository.update(current_user)
 
     result = {'message': 'Successfully changed password'}
     return jsonify(result), 200
-
-
-def _parse_arguments():
-    parser = RequestParser()
-    parser.add_argument('current_password')
-    parser.add_argument('new_password')
-    return parser.parse_args()
