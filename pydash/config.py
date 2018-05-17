138c864db3d7b904369974dd41c94261dabcc6a0
TWEApol <tom142606@gmail.com>
2018-05-17 18:30:28 +0200

    Refactored configuration settings.

--- pydash/config.py
+++ pydash/config.py
@@ -6,11 +6,13 @@ class Config(object):
         'SECRET_KEY') or 'rigorous-security-will-be-implemented-post-MVP'
 
     # Mail configurations
-    MAIL_SERVER         = 'smtp.gmail.com'
-    MAIL_PORT           = 465
-    MAIL_USE_TLS        = False  # True
-    MAIL_USE_SSL        = True
-    #MAIL_DEBUG          = True  # default = pydash_webapp.debug
-    MAIL_USERNAME       = 'noreply.PyDashTestMail@gmail.com'
-    MAIL_PASSWORD       = 'verysecurepydashpassword'
-    DEFAULT_MAIL_SENDER = 'noreply.PyDashTestMail@gmail.com'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    #MAIL_DEBUG = True  # default = pydash_webapp.debug
+    # MAIL_USERNAME       = 'noreply.PyDashTestMail@gmail.com'
+    # MAIL_PASSWORD       = 'verysecurepydashpassword'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    DEFAULT_MAIL_SENDER = 'noreply-PyDashTestMail <noreply.pydashtestmail@gmail.com>'
