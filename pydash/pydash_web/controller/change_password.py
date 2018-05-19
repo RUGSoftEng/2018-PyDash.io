0da83807e7de58c1d1746b14f9aea8d0af47a160
Koen Bolhuis <koen.bolhuis@gmail.com>
2018-05-19 17:10:20 +0200

    Fix password change

--- pydash/pydash_web/controller/change_password.py
+++ pydash/pydash_web/controller/change_password.py
@@ -6,6 +6,7 @@ from flask import jsonify
 from flask_login import current_user
 from flask_restplus.reqparse import RequestParser
 
+import pydash_app.user
 import pydash_app.user.repository as user_repository
 import pydash_logger
 
@@ -30,14 +31,21 @@ def change_password():
         result = {'message': 'Current password incorrect'}
         return jsonify(result), 401
 
+    actual_user = pydash_app.user.maybe_find_user(current_user.id)
+
+    if not actual_user:
+        logger.warning('Password change failed - current user does not exist for some reason')
+        result = {'message': 'Current user does not exist'}
+        return jsonify(result), 500
+
     try:
-        current_user.set_password(new_password)
+        actual_user.set_password(new_password)
     except ValueError:
         logger.warning("Password change failed - new password invalid")
         result = {'message': 'New password invalid'}
         return jsonify(result), 400
 
-    user_repository.update(current_user)
+    user_repository.update(actual_user)
 
     result = {'message': 'Successfully changed password'}
     return jsonify(result), 200
