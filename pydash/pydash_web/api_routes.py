bd8f8f2ab9cce9e8fd3b03547566f01a4230171f
TheVeggydude <jeroenlanghorst@gmail.com>
2018-05-21 16:22:57 +0200

    fixed duplicate method name in api_routes.py

--- pydash/pydash_web/api_routes.py
+++ pydash/pydash_web/api_routes.py
@@ -87,5 +87,5 @@ def get_visitor_heatmap(dashboard_id):
 
 @api.route("/api/dashboards/<dashboard_id>/unique_visitor_heatmap", methods=["GET"])
 @login_required
-def get_visitor_heatmap(dashboard_id):
+def get_unique_visitor_heatmap(dashboard_id):
     controller.visitor_heatmap(dashboard_id, 'unique_visitors')
