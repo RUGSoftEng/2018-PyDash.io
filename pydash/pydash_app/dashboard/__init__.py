f2647ad11e1d88623f890d7a837d38c72102d523
TheVeggydude <jeroenlanghorst@gmail.com>
2018-05-22 11:59:37 +0200

    added `verify_dashboard()` method to dashboard/__init__.py

--- pydash/pydash_app/dashboard/__init__.py
+++ pydash/pydash_app/dashboard/__init__.py
@@ -3,7 +3,13 @@ This module is the public interface (available to the web-application pydash_web
 for interacting with Dashboards.
 """
 from .entity import Dashboard
+from flask import jsonify
+from flask_login import current_user
+
+import pydash_logger
 import pydash_app.dashboard.repository
+
+logger = pydash_logger.Logger(__name__)
 
 
 def add_to_repository(dashboard):
@@ -36,3 +42,35 @@ def dashboards_of_user(user_id):
     :return: A list of Dashboard-entities.
     """
     return [db for db in repository.all() if db.user_id == user_id]
+
+
+def verify_dashboard(dashboard_id):
+    """
+    Verifies if a given dashboard_id is correct and if the current user has access
+    to the dashboard.
+    :param dashboard_id: The UUID of the dashboard to be validated.
+    :return: True if the dashboard is valid, else False followed by the result and
+    the http error code.
+    """
+
+    try:
+        dashboard = pydash_app.dashboard.find(dashboard_id)
+    except KeyError:
+        result = {'message': f'Dashboard_id {dashboard_id} is not found.'}
+        logger.warning(f'Endpoint_boxplots failure - Dashboard_id {dashboard_id} is not found. '
+                       f'Current user: {current_user}')
+        return False, jsonify(result), 400
+    except ValueError:  # Happens when called without a proper UUID
+        result = {'message': f'Dashboard_id {dashboard_id} is invalid UUID.'}
+        logger.warning(f'Endpoint_boxplots failure - Dashboard_id {dashboard_id} is invalid UUID. '
+                       f'Current user: {current_user}')
+        return False, jsonify(result), 400
+
+    # Check user authorisation
+    if str(dashboard.user_id) != str(current_user.id):
+        result = {'message': f'Current user is not allowed to access dashboard {dashboard_id}'}
+        logger.warning(f'Endpoint_boxplots failure - '
+                       f'User {current_user} is not allowed to access dashboard {dashboard_id}')
+        return False, jsonify(result), 403
+
+    return True, None, None
