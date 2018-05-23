66a0579f87b43811825f1a420741734219fd403a
Arjan Tilstra <a.tilstra.2@student.rug.nl>
2018-05-23 12:01:39 +0200

    Fix for 401

--- pydash-front/src/authenticated_app/dashboard/statistics_page/VisitorsHeatmapPanel.js
+++ pydash-front/src/authenticated_app/dashboard/statistics_page/VisitorsHeatmapPanel.js
@@ -15,29 +15,25 @@ class VisitorsHeatmapPanel extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            average_execution_time: 0,
-            total_execution_time: 0,
+            
         }
     }
 
     componentDidMount() {
         // TODO Once back-end has proper API endpoints, use that one instead of the overall one.
         let start_date = '2018-05-08';
-        axios.get(window.api_path + '/api/dashboards/' + this.props.dashboard_id + '/visitor_heatmap', {
-            start_date
-        },
+        axios.get(window.api_path + '/api/dashboards/' + this.props.dashboard_id + '/visitor_heatmap?start_date='+start_date,
             {withCredentials: true}
         ).then((response) => {
             console.log(response);
             this.setState(prevState => {
                 return {
                     ...prevState,
-                    average_execution_time: response.data.aggregates.average_execution_time,
-                    total_execution_time: response.data.aggregates.total_execution_time,
+                    
                 }
             });
         }).catch((error) => {
-            console.log('error while fetching dashboard information', error);
+            console.log('error while fetching heatmap information', error);
         });
     }
 
