09436938045162cac260f093cbbd22bd4d098129
TWEApol <tom142606@gmail.com>
2018-06-04 18:10:57 +0200

    Adds 'MAIL_PYDASH_HOST' and 'MAIL_PROTOCOL' as env.vars for use with deployment. Adds build_and_run[deployment].sh file that exports these env.vars automatically.

--- pydash/config.py
+++ pydash/config.py
@@ -5,8 +5,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'rigorous-security-will-be-implemented-post-MVP'
 
     # Mail configurations
-    # MAIL_SERVER = 'smtp.gmail.com'
-    # MAIL_PORT = 465
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
@@ -15,3 +13,6 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_DEFAULT_SENDER = ('PyDash', 'noreply.pydashtestmail@gmail.com')
+    MAIL_PYDASH_HOST = os.environ.get('MAIL_PYDASH_HOST') or 'localhost:5000'
+    # MAIL_PYDASH_PORT = os.environ.get('MAIL_PYDASH_PORT') or '5000'
+    MAIL_PROTOCOL = os.environ.get('MAIL_PROTOCOL') or 'http'
