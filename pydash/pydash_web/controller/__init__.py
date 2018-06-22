2314accaf3580f030d76bbfad123ec5f062c2aac
TWEApol <tom142606@gmail.com>
2018-06-22 20:30:24 +0200

    Separated dashboards and dashboard from pydash.web.controller.dashboards.py

--- pydash/pydash_web/controller/__init__.py
+++ pydash/pydash_web/controller/__init__.py
@@ -12,7 +12,7 @@ from .change_settings import change_settings
 from .change_password import change_password
 
 from .dashboards import dashboards
-from .dashboards import dashboard
+from .dashboard import dashboard
 from .register_dashboard import register_dashboard
 from .change_dashboard_settings import change_dashboard_settings
 from .delete_dashboard import delete_dashboard
